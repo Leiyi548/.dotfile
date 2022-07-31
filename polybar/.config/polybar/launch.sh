9e2ab3b3e72106d9a2a4b4e26c663f43a33eaf29
Leiyi548 <1424630446@qq.com>
2022-07-31 19:21:16 +0800

    update polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,6 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -r bar &
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar main --config=~/.config/polybar/config.ini &
+polybar second --config=~/.config/polybar/config.ini &
 
 echo "Polybar launched..."
