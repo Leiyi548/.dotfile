bbb16e2b6c9821f29e54a93a6c49383234f51876
Leiyi548 <1424630446@qq.com>
2022-08-15 22:22:04 +0800

    update i3

--- script/feh.sh
+++ script/feh.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
+feh --randomize --bg-fill ~/.dotfile/wallpaper/*.png
 while true; do
-	find ~/.dotfile/wallpaper -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
-		shuf -n1 -z | xargs -0 feh --bg-scale
-	# s 是秒 m是分钟
-	sleep 15m
+  feh --randomize --bg-fill ~/.dotfile/wallpaper/*.png
+  sleep 1000s
 done
 
