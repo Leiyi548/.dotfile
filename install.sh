282933a67fc7a9674e25aca2acbec38485cb620d
Leiyi548 <1424630446@qq.com>
2022-08-30 10:50:43 +0800

    添加字体，并添加安装字体的脚本

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+DIR=`pwd`
+FDIR="$HOME/.local/share/fonts"
+
+# insatll fonts
+
+install_fonts(){
+	echo -e "\n[*] Installing font ..."
+	if [[ -d "$FDIR" ]]; then
+		cp -rf $DIR/fonts/* "$FDIR"
+  else
+    mkdir -p "$FDIR"
+    cp -rf $DIR/fonts/* "$FDIR"
+}
+
+main(){
+  install_fonts
+}
+
+main
