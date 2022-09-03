7f2429e89b092538e16c8e85b6cede8b8ff76351
Leiyi548 <1424630446@qq.com>
2022-09-03 15:56:29 +0800

    update

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -342,11 +342,12 @@
 
   #####################################[ vcs: git status ]######################################
   # Branch icon. Set this parameter to '\UE0A0 ' for the popular Powerline branch icon.
-  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=
+  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=' '
 
   # Untracked files icon. It's really a question mark, your font isn't broken.
   # Change the value of this parameter to show a different icon.
   typeset -g POWERLEVEL9K_VCS_UNTRACKED_ICON='?'
+  # typeset -g POWERLEVEL9K_VCS_UNTRACKED_ICON='U'
 
   # Formatter for Git status.
   #
