897f68beaf079478870ae7538d8d7fd90bbf6c1f
Leiyi548 <1424630446@qq.com>
2022-09-20 17:47:41 +0800

    添加字体配置和安装

--- install.sh
+++ install.sh
@@ -5,13 +5,14 @@ FDIR="$HOME/.local/share/fonts"
 
 # insatll fonts
 
-install_fonts(){
+install_fonts() {
 	echo -e "\n[*] Installing font ..."
 	if [[ -d "$FDIR" ]]; then
 		cp -rf $DIR/fonts/* "$FDIR"
-  else
-    mkdir -p "$FDIR"
-    cp -rf $DIR/fonts/* "$FDIR"
+	else
+		mkdir -p "$FDIR"
+		cp -rf $DIR/fonts/* "$FDIR"
+	fi
 }
 
 main(){
