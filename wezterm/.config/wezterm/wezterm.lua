4ced3c545fd53c6dd274e36e6327082125510537
Leiyi548 <1424630446@qq.com>
2022-11-03 17:44:30 +0800

    use wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,15 +2,17 @@
 local wt = require("wezterm")
 
 local function get_theme()
+	-- more color_scheme please see https://wezfurlong.org/wezterm/colorschemes/index.html
 	local _time = os.date("*t")
-	if _time.hour >= 1 and _time.hour < 9 then
-		return "Rosé Pine (base16)"
-	elseif _time.hour >= 9 and _time.hour < 17 then
+	if _time.hour >= 8 and _time.hour < 16 then
+		-- return "tokyonight_night"
+		return "Catppuccin Latte" -- light theme
+	else
+		-- return "Catppuccin Frappe" -- Macchiato Frappe Mocha
 		return "tokyonight_night"
-	elseif _time.hour >= 17 and _time.hour < 21 then
-		return "Catppuccin Mocha"
-	elseif _time.hour >= 21 and _time.hour < 24 or _time.hour >= 0 and _time.hour < 1 then
-		return "kanagawabones"
+		-- return "Catppuccin Latte" -- light theme
+		-- return "kanagawabones"
+		-- return "Rosé Pine (base16)"
 	end
 end
 
@@ -22,8 +24,8 @@ return {
 		bottom = 0,
 	},
 	enable_tab_bar = false,
-	font = wt.font("Operator Mono Lig", { weight = 325 }),
-	font_size = 16,
+	font = wt.font("JetBrainsMono Nerd Font", { weight = 325 }),
+	font_size = 15,
 	bold_brightens_ansi_colors = true,
 	color_scheme = get_theme(),
 	custom_block_glyphs = true,
@@ -32,6 +34,6 @@ return {
 	default_cursor_style = "SteadyBar",
 	cursor_blink_ease_in = "Constant",
 	cursor_blink_ease_out = "Constant",
-	enable_csi_u_key_encoding = true,
-	window_background_opacity = 0.8,
+	enable_csi_u_key_encoding = false,
+	window_background_opacity = 0.9,
 }
